df6d344e0966dad2e4563b42793767fe719d05b9
sanneduinkerx <sanne.duinker@hva.nl>
2021-04-13 15:07:53 +0200

    template ejs views + style chat changed

--- server.js
+++ server.js
@@ -5,11 +5,22 @@ const app = express();
 // http server on web, listens to port, express linked to port with data
 //process listens to port with data from app
 const http = require('http').createServer(app);  // read into http
-const path = require('path');
 const io = require('socket.io')(http); // read into (http)
 
-//looks in public folder for path
-app.use(express.static(path.resolve('public')));
+//looks in public folder for static files
+app.use(express.static('public'));
+app.set('view engine', 'ejs'); 
+
+//routes -> in different file later 
+app.get('/', function (req, res) {
+    //the (res) respond: renders ejs template home, from the view folder
+    res.render('home');
+})
+
+app.get('/game', function (req, res) {
+    //the (res) respond: renders ejs template home, from the view folder
+    res.render('game');
+})
 
 // when a connection is made with socket.io, the following function gets executed
 // the parameter socket is given with the function
@@ -20,7 +31,6 @@ io.on('connection', (socket) => {
     // self named event, message from client side
     // event is given in parameter in nameless function
     socket.on('message', (message) => {
-        console.log(`Message: ${message}`); 
         //io emit to SEND the message back to all clients that have browser open
         io.emit('message', message);
     })
